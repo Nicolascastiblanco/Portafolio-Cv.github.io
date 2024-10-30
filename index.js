9dad72408d828da4c3b9a2fcd517b84e37399931
Nicolascastiblanco <ni-vanico@hotmail.com>
2024-10-29 23:40:02 -0500

    se actializo el sobre mi y la imagen rota

---
+++ index.js
@@ -0,0 +1,6 @@
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const image = document.querySelector(".presentacion__imagen");
+        image.classList.add("animate-float");
+    });
+
